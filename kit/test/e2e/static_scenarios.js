9ce17f288da729f3405a2025259884fcb093e4c4
krukmat <kruk.matias@gmail.com>
2012-03-05 14:55:59 -0300

    News: check

--- kit/test/e2e/static_scenarios.js
+++ kit/test/e2e/static_scenarios.js
@@ -147,3 +147,22 @@ describe('Additinal test from Ivan', function() {
     expect(element('#contributorsAboutBoxText > a > img:eq(4)').attr('alt')).toBe('Contributor Jeffery');
   });
 });
+
+describe('Additinal test from Matias', function() {
+	  it('Testing alex/news.html', function() {
+	    browser().navigateTo('../../news.html');
+	    expect(element('#menuOptionsText > .menuSelected').text()).toBe('news');
+	    expect(element('#contributorsInfoBoxText > p').text()).toBe('News');
+	    
+	    // Test the content of the contributors right menu
+	    expect(element('#contributorsAboutBoxText > a > img').count()).toBe(5);
+	    expect(repeater('#contributorsAboutBoxText > a > img'))
+	    
+	    // To be organized as repeater when same been used
+	    expect(element('#contributorsAboutBoxText > a > img:eq(0)').attr('alt')).toBe('Contributor Danny');
+	    expect(element('#contributorsAboutBoxText > a > img:eq(1)').attr('alt')).toBe('Contributor Chris Meyers');
+	    expect(element('#contributorsAboutBoxText > a > img:eq(2)').attr('alt')).toBe('Contributor Allen Downey');
+	    expect(element('#contributorsAboutBoxText > a > img:eq(3)').attr('alt')).toBe('Contributor Chris Boesch');
+	    expect(element('#contributorsAboutBoxText > a > img:eq(4)').attr('alt')).toBe('Contributor Jeffery');
+	  });
+	});
