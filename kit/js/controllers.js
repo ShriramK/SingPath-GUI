07085626c6a2c9024f4a93fb75035333977bf4b0
krukmat <kruk.matias@gmail.com>
2012-03-07 17:21:47 -0300

    Custom static scenarios. badges.html test. Badges moving to AngularJS.

--- kit/js/controllers.js
+++ kit/js/controllers.js
@@ -15,3 +15,43 @@ function RankingCtrl($resource) {
 }
 
 RankingCtrl.$inject = ["$resource"];
+
+function YourLevelBadgesCtrl($resource) {	
+    yourLevelBadgesModel = $resource("../jsonapi/all_badges");
+    this.badges = yourLevelBadgesModel.get();
+    this.doFilter = function(elem) { 
+        return (elem.class.indexOf('CountryBadge')<0) && (elem.class.indexOf('Level_Badge')<0);
+    }
+    //this.badges_level = this.badges.badges.filter({'description':'Argentina Unlock Badge'});
+}
+
+YourLevelBadgesCtrl.$inject = ["$resource"];
+
+function CountryLevelBadgesCtrl($resource) {	
+    countryLevelBadgesModel = $resource("../jsonapi/all_badges");
+    this.badges = countryLevelBadgesModel.get();
+    this.doFilter = function(elem) { 
+        return elem.class.indexOf('CountryBadge')>0;
+    }
+ }
+
+CountryLevelBadgesCtrl.$inject = ["$resource"];
+
+function YourBadgesBoxTop($resource) {	
+	yourBadgesBoxTop = $resource("../jsonapi/all_badges");
+    this.badges = yourBadgesBoxTop.get();
+    this.doFilter = function(elem) { 
+        return elem.class.indexOf('Level_Badge')>0;
+    }
+    
+ }
+
+YourBadgesBoxTop.$inject = ["$resource"];
+
+
+function CountriesCtrl($resource) {	
+    allCountriesModel = $resource("../jsonapi/all_countries");
+    this.allCountries = allCountriesModel.get();
+}
+
+CountriesCtrl.$inject = ["$resource"];
