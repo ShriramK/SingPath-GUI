175f06c939d806b42b391d6f2d8d8d09ac32742b
soulovbelial <fbsd4ever@gmail.com>
2012-03-13 13:47:25 +0200

    Update kit/test/e2e/static_scenarios.js

--- kit/test/e2e/static_scenarios.js
+++ kit/test/e2e/static_scenarios.js
@@ -143,12 +143,12 @@ describe('Tests From SergeyGalenko', function() {
       //expect(element('#footer a:nth-child(1)').text()).toBe('home');
       element('#viewRanking .viewRankingButton').click();
       expect(browser().location().path()).toEqual('/kit/tournamentRanking.html?tournamentID=11288841');
-      expect(browser().location().href()).toEqual('/kit/tournamentRanking.html?tournamentID=11288841');
+      expect(browser().location().href).toEqual('/kit/tournamentRanking.html?tournamentID=11288841');
       expect(browser().location().hashPath()).toEqual('/kit/tournamentRanking.html?tournamentID=11288841');
       expect(browser().location().hash()).toEqual('/kit/tournamentRanking.html?tournamentID=11288841');
       
       expect(browser().location().path()).toBe('/kit/tournamentRanking.html?tournamentID=11288841');
-      expect(browser().location().href()).toBe('/kit/tournamentRanking.html?tournamentID=11288841');
+      expect(browser().location().href).toBe('/kit/tournamentRanking.html?tournamentID=11288841');
       expect(browser().location().hashPath()).toBe('/kit/tournamentRanking.html?tournamentID=11288841');
       expect(browser().location().hash()).toBe('/kit/tournamentRanking.html?tournamentID=11288841');
       
