64c3b61b2269162525f198e7a4a21099fabfec89
krukmat <kruk.matias@gmail.com>
2012-03-14 18:03:10 -0300

    SingaporeController

--- kit/js/controllers.js
+++ kit/js/controllers.js
@@ -256,6 +256,46 @@ function WorldWideRankingCtrl($resource){
 
 WorldWideRankingCtrl.$inject = ["$resource"];
 
+function SingaporeRankingCtrl($resource){
+	worldWideRanking = $resource('../jsonapi/worldwide_ranking');
+	this.worldWideRanking = worldWideRanking.get();
+	this.ranking = [];
+	this.checkLast = function(elem){
+		var index = this.ranking.indexOf(elem);
+		if (index == 24)
+			return "UR";
+		width = 2;
+		number = elem.rank;
+		width -= number.toString().length;
+		if ( width > 0 )
+		  {
+		    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+		  }
+		return number;
+	}
+	this.addZeros = function(elem){
+		width = 2;
+		number = elem.rank;
+		width -= number.toString().length;
+		if ( width > 0 )
+		  {
+		    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+		  }
+		return number;
+	}
+	this.doFilter = function(elem) {
+    	//elem.imageURL = elem.imageURL.replace(/^\/static/, "../static");;
+        var isSingapore = elem.playerCountry.countryName=="Singapore";
+        if (isSingapore){
+        	this.ranking.push(elem);
+        	return true;
+        }
+        return false;
+    }
+}
+
+WorldWideRankingCtrl.$inject = ["$resource"];
+
 
 function HeadMenuOptionsCtrl($resource, $location) {
   // Taking all menu options
