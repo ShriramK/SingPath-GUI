69242bfd592da29f7a7b21c5fc10529591a4e0d0
IvanDimanov <IvanDimanov1@gmail.com>
2012-03-21 22:00:58 +0200

    update home.html code update panel.html includes create profile_panel.html panel create badges_panel.html panel

--- kit/js/home.js
+++ kit/js/home.js
@@ -6,10 +6,16 @@ function ProfilePanelCtrl($resource) {
   this.btnTitle       = "Edit Details";
   this.label          = "Profile";
   
-  this.profile = $resource('../jsonapi/player_test').get(function() {
-    that.profile.professional = that.profile.professional ? 'professional' : 'student';
-    that.profile.countrySrc   = '../static/flags/'+ that.profile.countryCode.toLowerCase() +'_on.png';
-    that.profile.genderSrc    = '../kit/_images/commonButtons/genderIcon'+ that.profile.gender.capitalFirstLetter() +'_off.png';
+  this.profile = $resource('../jsonapi/player_test').get(function(profile) {
+    that.profile = profile;
+    
+    // Setting a global user id
+    window.USER.id = profile.player_id;
+    
+    // Setting profile data
+    that.profile.professional = profile.professional*1 ? 'professional' : 'student';
+    that.profile.countrySrc   = '../static/flags/'+ profile.countryCode.toLowerCase() +'_on.png';
+    that.profile.genderSrc    = '../kit/_images/commonButtons/genderIcon'+ profile.gender.capitalFirstLetter() +'_off.png';
   });
 }
 
@@ -18,15 +24,31 @@ ProfilePanelCtrl.$inject = ['$resource'];
 
 function BadgesPanelCtrl($resource) {
   that                = this;
-  this.containerClass = "profileContainer";
-  this.btnTitle       = "Edit Details";
-  this.label          = "Profile";
+  this.containerClass = "badgesContainer";
+  this.btnTitle       = "View Badges";
+  this.label          = "Badges";
   
-  this.profile = $resource('../jsonapi/player_test').get(function() {
-    that.profile.professional = that.profile.professional ? 'professional' : 'student';
-    that.profile.countrySrc   = '../static/flags/'+ that.profile.countryCode.toLowerCase() +'_on.png';
-    that.profile.genderSrc    = '../kit/_images/commonButtons/genderIcon'+ that.profile.gender.capitalFirstLetter() +'_off.png';
-  });
+  // Check if some controller have alreasy load the user id
+  if(window.USER.id) {
+    loadUserBadges(window.USER.id);
+  } else {
+    $resource('../jsonapi/player_test').get(function(profile) {
+      // Setting a global user id
+      window.USER.id = profile.player_id;
+      
+      loadUserBadges(window.USER.id);
+    });
+  }
+  
+  // Load all user badges regardin sent user id
+  function loadUserBadges(id) {
+    that.badgesResource = $resource('../jsonapi/badges_for_current_player/'+ id).get(function(badgesResource) {
+      that.badgesResource = badgesResource;
+      
+      that.badges_type = that.badgesResource.type;
+      that.badges      = that.badgesResource.badges;
+    });
+  }
 }
 
 BadgesPanelCtrl.$inject = ['$resource'];
