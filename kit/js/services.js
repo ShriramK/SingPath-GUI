1dc64fa687358b180798f1dd39f0e9078e5400b4
Chris Boesch <scboesch@gmail.com>
2012-03-30 18:50:19 +0800

    Moved content for kit/index.html into a partial. Moving towards a single page design for pages in the menu bar.

--- kit/js/services.js
+++ kit/js/services.js
@@ -5,21 +5,22 @@
  */
 angular.service('myAngularApp', function($route, $location, $window) {
 
-  $route.when('/home', {template: 'includes/home.html', controller: IndexStatsPageCtr});
-  //$route.when('/view1', {template: 'partials/partial1.html', controller: MyCtrl1});
+  $route.when('', {template: 'includes/index_partial.html', controller:LoadPageCtrl});
+  $route.when('/home', {template: 'includes/home.html', controller:LoadPageCtrl});
+  $route.when('/ranking', {template: 'includes/home.html', controller:LoadPageCtrl});
   //$route.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
 
   var self = this;
 
-  //$route.onChange(function() {
-  //  if ($location.hash === '') {
-  //    $location.updateHash('/home');
+  $route.onChange(function() {
+    if ($location.hash === '') {
+      $location.updateHash('');
       //self.$eval();
-  //  } 
-    //else {
-    //  $route.current.scope.params = $route.current.params;
-    //  $window.scrollTo(0,0);
-    //}
-  //});
+    } 
+    else {
+      //$route.current.scope.params = $route.current.params;
+      //$window.scrollTo(0,0);
+    }
+  });
 
 }, {$inject:['$route', '$location', '$window'], $eager: true});
