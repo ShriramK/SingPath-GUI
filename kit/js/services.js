836d30e7f7f7a1e6518685ecfbf6685a2e72938e
Chris Boesch <scboesch@gmail.com>
2012-03-30 14:50:13 +0800

    Updating pages to reference angular.min.js to enable easier library migrations.

--- kit/js/services.js
+++ kit/js/services.js
@@ -5,19 +5,21 @@
  */
 angular.service('myAngularApp', function($route, $location, $window) {
 
-  $route.when('/view1', {template: 'partials/partial1.html', controller: MyCtrl1});
-  $route.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
+  $route.when('/home', {template: 'includes/home.html', controller: IndexStatsPageCtr});
+  //$route.when('/view1', {template: 'partials/partial1.html', controller: MyCtrl1});
+  //$route.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
 
   var self = this;
 
-  $route.onChange(function() {
-    if ($location.hash === '') {
-      $location.updateHash('/view1');
-      self.$eval();
-    } else {
-      $route.current.scope.params = $route.current.params;
-      $window.scrollTo(0,0);
-    }
-  });
+  //$route.onChange(function() {
+  //  if ($location.hash === '') {
+  //    $location.updateHash('/home');
+      //self.$eval();
+  //  } 
+    //else {
+    //  $route.current.scope.params = $route.current.params;
+    //  $window.scrollTo(0,0);
+    //}
+  //});
 
 }, {$inject:['$route', '$location', '$window'], $eager: true});
