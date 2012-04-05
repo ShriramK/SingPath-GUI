84ff2b67753d43e4c678ca73e4475e44510bfa05
IvanDimanov <IvanDimanov1@gmail.com>
2012-04-05 17:41:45 +0300

    fix merge errors update common HTML and CSS code implement a basic level of routing

--- kit/js/services.js
+++ kit/js/services.js
@@ -3,24 +3,8 @@
 /**
  * App service which is responsible for the main configuration of the app.
  */
-angular.service('myAngularApp', function($route, $location, $window) {
-
-  $route.when('', {template: 'includes/index_partial.html', controller:LoadPageCtrl});
-  $route.when('/home', {template: 'includes/home.html', controller:LoadPageCtrl});
-  $route.when('/ranking', {template: 'includes/home.html', controller:LoadPageCtrl});
-  //$route.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
-
-  var self = this;
-
-  $route.onChange(function() {
-    if ($location.hash === '') {
-      $location.updateHash('');
-      //self.$eval();
-    } 
-    else {
-      //$route.current.scope.params = $route.current.params;
-      //$window.scrollTo(0,0);
-    }
-  });
-
-}, {$inject:['$route', '$location', '$window'], $eager: true});
+function RouterCtrl($route) {
+  $route.when('',         {template: 'includes/index_partial.html', controller: LoadPageCtrl});
+  $route.when('/home',    {template: 'includes/home.html',          controller: LoadPageCtrl});
+  $route.when('/ranking', {template: 'includes/home.html',          controller: LoadPageCtrl});
+}
